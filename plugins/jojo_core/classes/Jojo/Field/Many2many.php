168808eae441bd4f65fc7bd13ef814ee7b6fd6cd
Mike Cochrane <mikec@mikenz.geek.nz>
2010-07-29 04:33:17 +0000

    Make many 2 many field types work where the link table is grouped. And delete half the code as it did nothing and made it look complicated

--- plugins/jojo_core/classes/Jojo/Field/Many2many.php
+++ plugins/jojo_core/classes/Jojo/Field/Many2many.php
@@ -49,14 +49,9 @@
 //////////////////////MANY2MANYFIELD//////////////////////
 class Jojo_Field_many2many extends Jojo_Field
 {
-    var $rows;
-    var $options;
-
     function __construct($fielddata = array())
     {
         parent::__construct($fielddata);
-        $this->rows = 1; //TODO: make this optional in Fielddata
-        $this->options = array();
     }
 
     function checkvalue()
@@ -69,73 +64,51 @@ class Jojo_Field_many2many extends Jojo_Field
 
     function displayedit()
     {
-        $output = '';
         $itemid = $this->table->getRecordID();
+        $tree = new hktree('tree');
 
-        /* get all current selections */
-        $categories = Jojo::selectQuery("SELECT * FROM {" . $this->linktable . "} WHERE `" . $this->linkitemid . "` = ?", array($itemid));
-        $selections = array(); //simple array holding all the categories the item is assigned to
-        $n = count($categories);
-        for ($i=0;$i<$n;$i++) {
-            $selections[] = $categories[$i][$this->linkcatid];
+        /* Get all the current selections */
+        $selections = Jojo::selectAssoc("SELECT " . $this->linkcatid . " as `key`, " . $this->linkcatid . " as `value` FROM {" . $this->linktable . "} WHERE `" . $this->linkitemid . "` = ?", array($itemid));
+
+        /* Add group by level one */
+        $tableoptions = Jojo::selectRow("SELECT * FROM {tabledata} WHERE td_name = ? LIMIT 1", array($this->cattable));
+        if ($tableoptions['td_group1'] != "") {
+            $layer1 = Jojo::selectAssoc("SELECT " . $tableoptions['td_group1'] . ' as `key`, '  . $tableoptions['td_group1']." as `value` FROM {".$tableoptions['td_name']."} GROUP BY ".$tableoptions['td_group1']." ORDER BY ".$tableoptions['td_group1']."");
+            foreach($layer1 as $k => $v) {
+                $tree->addNode($v, 0, '<strong>' . $v . '</strong>');
+            }
         }
 
-        $tablename = $this->cattable;
-        $rows = Jojo::selectQuery("SELECT * FROM {tabledata} WHERE td_name = ? LIMIT 1", array($tablename));
-        $tableoptions = $rows[0];
-
-        $idfield        = $tableoptions['td_primarykey'];
+        /* Main query */
         $displayfield   = Jojo::either($tableoptions['td_displayfield'], $tableoptions['td_primarykey']);
         $parentfield    = Jojo::either($tableoptions['td_parentfield'], $tableoptions['td_group1'], "'0'");
-        $categorytable  = $tableoptions['td_categorytable'];
-        $categoryfield  = Jojo::either($tableoptions['td_categoryfield'], $tableoptions['td_group1'], "'0'");
         $orderbyfield   = $tableoptions['td_orderbyfields'];
-        $group1field    = $tableoptions['td_group1'];
-
-        /* TODO - Add group2 logic */
-        $datafilter =  Jojo::either($tableoptions['td_filterby'],'1'); //filter results
-        $rolloverfield =  Jojo::either($tableoptions['td_rolloverfield'],"''");
-        $html = ''; //this will be used for output
-
-        //Layer1 represents table structure where the first level is the grouping, then individual records underneath
-        if ($tableoptions['td_group1'] != "") { // - this is used to pull down the main groupings - takes an extra query
-            $layer1 = Jojo::selectQuery("SELECT ".$tableoptions['td_group1']." FROM {".$tableoptions['td_name']."} GROUP BY ".$tableoptions['td_group1']." ORDER BY ".$tableoptions['td_group1']."");
-        }
-        /* Main query */
-        $query = "SELECT $idfield AS id, $displayfield AS display, $parentfield AS parent, $categoryfield AS categoryfield, $rolloverfield AS rollover ". Jojo::onlyIf($group1field,",".$group1field." AS group1").
-                    ' FROM {'.$tableoptions['td_name']."}
-                    WHERE $datafilter ".
-                    ' ORDER BY '. Jojo::onlyIf($group1field,' '.$group1field.', ').
+        $query = "SELECT
+                    " . $tableoptions['td_primarykey'] . " AS id,
+                    $displayfield AS display,
+                    $parentfield AS parent
+                 FROM
+                    {" . $tableoptions['td_name'] . "}
+                 ORDER BY
+                  ". Jojo::onlyIf($tableoptions['td_group1'],' '.$tableoptions['td_group1'].', ').
                      Jojo::onlyIf($orderbyfield,' '.$orderbyfield.', ').
                      Jojo::onlyIf($displayfield,' '.$displayfield.', ').
                     ' display';
-
         $records = Jojo::selectQuery($query);
 
-        $i = 1;
+        /* Create the html */
         foreach ($records as $record) {
-            $options[$i]['name']        = $record['display'];
-            $options[$i]['value']       = $record['id'];
-            $options[$i]['parent']      = $record['parent'];
-            $this->options[$i]['group'] = isset($record['group1']) ? $record['group1'] : '';
-            $i++;
+            $item = sprintf("<label><input type='checkbox' name='fm_%s_%s' id='fm_%s_%s' value='%s' onchange='fullsave = true;'%s />%s</label><br />\n",
+                        $this->fd_field,
+                        $record['id'],
+                        $this->fd_field,
+                        $record['id'],
+                        $record['id'],
+                        (in_array($record['id'], $selections) ? ' checked="checked"' : ''),
+                        $record['display']);
+            $tree->addNode($record['id'], $record['parent'], $item);
         }
-
-        $tree = new hktree('tree');
-
-        /* loop through each option and display */
-        $n = count($options);
-        for ($i=1;$i<=$n;$i++) {
-            $isselected = in_array($options[$i]['value'], $selections) ? ' checked="checked"' : '';
-            $item = "<label><input type='checkbox' name=\"fm_".$this->fd_field."_".$options[$i]['value']."\" id=\"fm_".$this->fd_field."_".$options[$i]['value']."\" value=\"".$options[$i]['value']."\" onchange=\"fullsave = true;\"".$isselected." /> ".$options[$i]['name']."</label><br />\n";
-
-            if (empty($categoryfield)) {
-                $tree->addNode($options[$i]['value'], $options[$i]['parent'], $item);
-            } else {
-                $tree->addNode($options[$i]['value'], $options[$i]['parent'], $item);
-            }
-        }
-        $output .= '<input type="hidden" name="fm_'.$this->fd_field.'" value="1" />'.$tree->printout_plain();
+        $output = '<input type="hidden" name="fm_'.$this->fd_field.'" value="1" />'.$tree->printout_plain();
 
         return $output;
     }
