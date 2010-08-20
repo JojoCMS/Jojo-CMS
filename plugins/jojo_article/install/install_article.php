c2aa1585974f0e1270d4325b08e1c2297503ee87
Michael Brandon <michael@searchmasters.co.nz>
2010-08-20 12:07:35 +0000

    mod

--- plugins/jojo_article/install/install_article.php
+++ plugins/jojo_article/install/install_article.php
@@ -97,6 +97,7 @@ $query = "
       `type` enum('normal','parent','index') NOT NULL default 'normal',
       `sortby` enum('ar_title asc','ar_date desc','ar_livedate desc','ar_author') NOT NULL default 'ar_date desc',
       `weighting` tinyint(1) default '1',
+      `externalrsslink` varchar(255) NOT NULL default '',
       `rsslink` tinyint(1) default '1',
       `thumbnail` varchar(255) NOT NULL default '',";
 if (class_exists('Jojo_Plugin_Jojo_comment')) {
