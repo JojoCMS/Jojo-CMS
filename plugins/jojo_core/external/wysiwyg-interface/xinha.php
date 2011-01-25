b9ef30e6c6fd3390dbf78c1a0bbf5c6906a5b52c
Tom Dale <tom@zero.co.nz>
2011-01-25 03:18:59 +0000

    update external repo for smarty

--- plugins/jojo_core/external/wysiwyg-interface/xinha.php
+++ plugins/jojo_core/external/wysiwyg-interface/xinha.php
@@ -26,7 +26,7 @@
       xinha_plugins = xinha_plugins ? xinha_plugins :
       [
         /* Load plugins */
-                  <?php 
+                  <?php
                     $xinha_plugins = array (
                         'ContextMenu',
                         'Stylist',
@@ -37,9 +37,9 @@
                         'InsertAnchor',
                         'HtmlEntities'
                     );
-                    
+
                     $xinha_plugins = $sitemap = Jojo::applyFilter('xinha_plugins', $xinha_plugins);
-                    
+
                     foreach ($xinha_plugins as $plugin) {
                         echo "'$plugin',\n";
                     }
@@ -76,15 +76,28 @@
         xinha_config.baseHref = "<?php echo _SITEURL ?>/";
         xinha_config.sevenBitClean = false;
 
+        xinha_config.formatblock =
+          {
+            "&mdash; format &mdash;": "",
+            "Heading 1": "h1",
+            "Heading 2": "h2",
+            "Heading 3": "h3",
+            "Heading 4": "h4",
+            "Heading 5": "h5",
+            "Heading 6": "h6",
+            "Normal"   : "p",
+            "Block Quote"  : "blockquote",
+            "Formatted": "pre"
+          };
         <?php $xinhastriphref = Jojo::getOption('xinha_strip_href','yes');
-        if($xinhastriphref=='no'){ ?>	
-                xinha_config.stripBaseHref = false;	
+        if($xinhastriphref=='no'){ ?>
+                xinha_config.stripBaseHref = false;
         <?php } ?>
         if (xinha_config.ExtendedFileManager) {
                 with (xinha_config.ExtendedFileManager)
                 {
                     <?php
-	
+
                     // define backend configuration for the plugin
                     $IMConfig = array();
                     $IMConfig['images_dir'] = _DOWNLOADDIR . '/images/';
@@ -98,24 +111,24 @@
                     $IMConfig['tmp_prefix'] = '_tmp';
                     $IMConfig['view_type'] =  Jojo::getOption('xinha_viewtype','thumbview');
                     $IMConfig['allow_upload'] = true;
-                    $IMConfig['max_filesize_kb_image'] = Jojo::getOption('max_imageupload_size','2000');           
+                    $IMConfig['max_filesize_kb_image'] = Jojo::getOption('max_imageupload_size','2000');
                     $IMConfig['max_filesize_kb_link'] = Jojo::getOption('max_fileupload_size','5000');
-            
+
                     // Maximum upload folder size in Megabytes.
                     // Use 0 to disable limit
                     $IMConfig['max_foldersize_mb'] = 0;
-            
+
                     $IMConfig['allowed_image_extensions'] = array("jpg","gif","png");
                     $IMConfig['allowed_link_extensions'] = array("jpg","gif","pdf","ip","txt","doc",
                                                                  "psd","png","html","swf",
                                                                  "xml","xls");
-            
+
                     require_once _BASEPLUGINDIR . '/jojo_core/external/xinha/contrib/php-xinha.php';
                     xinha_pass_to_php_backend($IMConfig);
                     ?>
                 }
         }
-	
+
   xinha_editors = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);
 
   xinha_editors.myTextArea.config.width = '780px';
