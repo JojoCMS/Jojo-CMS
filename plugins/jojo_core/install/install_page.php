95cc69ded89d4f5f1cb93407766ab8f31c3de74f
Tom Dale <tom@zero.co.nz>
2013-11-01 04:27:47 +0000

    Core: Allow long titles

--- plugins/jojo_core/install/install_page.php
+++ plugins/jojo_core/install/install_page.php
@@ -33,7 +33,7 @@ $table = 'page';
 $query = "
     CREATE TABLE {page} (
       `pageid` int(11) NOT NULL auto_increment,
-      `pg_title` varchar(100) NOT NULL default '',
+      `pg_title` varchar(255) NOT NULL default '',
       `pg_menutitle` varchar(100) NOT NULL default '',
       `pg_desc` varchar(255) NOT NULL default '',
       `pg_seotitle` varchar(255) NOT NULL default '',
